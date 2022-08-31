1d89f7d10e8ad12f568b2921350ccfe677345b2d
vishwadeaero <vishwadeaero@gmail.com>
2022-08-31 23:39:05 +0530

    Slider Added

---
+++ src/data.jsx
@@ -0,0 +1,23 @@
+export const sliderItems = [
+    {
+        id: 1,
+        img: "https://i.picsum.photos/id/831/1080/720.jpg?hmac=pQjjcK87GiX9FBGI1xRzRr1Ddv3up98iJJOgjKQ14GE",
+        title: "SUMMER SALE",
+        desc: "LOREM IPSUM DOLOR SI AMET",
+        bg: "#f5ebb1"
+    },
+    {
+        id: 2,
+        img: "https://i.picsum.photos/id/736/1080/720.jpg?hmac=Nm2BnBaDsNDaP5_KFjEstTEKXw86G2LN5R8oXUohZIQ",
+        title: "WINTER SALE",
+        desc: "LOREM IPSUM DOLOR SI AMET",
+        bg: "#b1f5ec"
+    },
+    {
+        id: 3,
+        img: "https://i.picsum.photos/id/900/1080/720.jpg?hmac=JF33gEySBJkm6DDjS69j8DZY5hyN7ubevSEQOK5K-oQ",
+        title: "AUTUMN SALE",
+        desc: "LOREM IPSUM DOLOR SI AMET",
+        bg: "#b3f5b1"
+    },
+]
